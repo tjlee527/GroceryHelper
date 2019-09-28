ceaa9caccad658f19cef0b6744f3030618ed936f
tjlee527 <tjlee527@gmail.com>
2019-09-28 13:28:03 -0700

    set up mongo

---
+++ database/index.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/groceryList', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+const Schema = mongoose.Schema;
+
+db.on('error', function() {
+  console.log('mongoose connection error');
+});
+
+db.once('open', function() {
+  console.log('mongoose connected successfully');
+});
+
+const itemSchema = new Schema({
+  item: String,
+  type: String,
+  price: Number, 
+  required: String,
+});
+
+const Items = mongoose.model('Items', itemSchema);
